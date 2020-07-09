01f3c3ddf4b75e7c0b2b08dbfa3d696a8cb205e7
Jillo Abdullahi <jillo.abdulla@gmail.com>
2020-07-09 14:23:04 +0300

    ts types

---
+++ sandbox.js
@@ -0,0 +1,11 @@
+var character = "jillo";
+var age = 30;
+var isBlackBelt = false;
+// character = 20
+character = "luigi";
+// age = 'yoshi'
+age = 55;
+// isBlackBelt = "yes"
+isBlackBelt = true;
+var circ = function (diameter) { return (diameter * Math.PI); };
+console.log(circ(7.434));
---
+++ sandbox.ts
@@ -0,0 +1,23 @@
+let character = "jillo";
+let age = 30;
+let isBlackBelt = false;
+
+
+// character = 20
+character = "luigi"
+
+// age = 'yoshi'
+age = 55;
+
+// isBlackBelt = "yes"
+isBlackBelt = true;
+
+const circ = (diameter: number) => (
+    diameter * Math.PI
+)
+
+console.log(circ(7.434));
+
+
+
+
