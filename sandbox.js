8098336a07b98a7b5f09643fb76768df0a1ac8da
Jillo Abdullahi <jillo.abdulla@gmail.com>
2020-07-09 16:47:50 +0300

    union types and explicit declarations

--- sandbox.js
+++ sandbox.js
@@ -16,3 +16,35 @@ ninja.name = "Jillo";
 //     age: 43,
 //     skills: ["Skiing","bungee"]
 // }
+// explicit types
+var character;
+var age;
+var isLoggedIn;
+age = 30;
+isLoggedIn = false;
+// let ninjas: string[];
+// ninjas = ["marion","jillo"]
+// console.log(ninjas)
+// ninjas.push("JILL");
+//union types
+var mixed = [];
+mixed.push("yellow");
+mixed.push(20);
+mixed.push(true);
+console.log(mixed);
+var uid;
+uid = 20;
+uid = "yellow";
+//objects
+var ninjaOne = { name: "jill" };
+ninjaOne = {
+    name: "Jillo",
+    age: 23
+};
+var ninjaTwo;
+ninjaTwo = {
+    name: "Jillo",
+    age: 23,
+    beltColor: "red"
+};
+console.log(ninjaTwo);
--- sandbox.ts
+++ sandbox.ts
@@ -21,3 +21,50 @@ ninja.name = "Jillo";
 //     skills: ["Skiing","bungee"]
 // }
 
+// explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+age = 30;
+isLoggedIn = false;
+
+
+// let ninjas: string[];
+// ninjas = ["marion","jillo"]
+// console.log(ninjas)
+
+// ninjas.push("JILL");
+
+//union types
+let mixed: (string|number|boolean)[] = [];
+mixed.push("yellow")
+mixed.push(20);
+mixed.push(true)
+
+console.log(mixed)
+
+let uid: number|string;
+uid = 20;
+uid = "yellow"
+
+//objects
+let ninjaOne: object = {name: "jill"};
+ninjaOne = {
+    name: "Jillo",
+    age: 23
+}
+
+let ninjaTwo: {
+    name: string,
+    age: number,
+    beltColor: string
+}
+
+ninjaTwo = {
+    name: "Jillo",
+    age: 23,
+    beltColor: "red"
+}
+
+console.log(ninjaTwo)
